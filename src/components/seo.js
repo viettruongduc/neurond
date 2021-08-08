7316c36cb86b74a3c693ba7bf5514028a9093b83
viettruongduc <vietdt301@gmail.com>
2021-08-08 22:10:10 +0700

    fix bug

--- src/components/seo.js
+++ src/components/seo.js
@@ -8,9 +8,9 @@
  import React from "react"
  import PropTypes from "prop-types"
  import { Helmet } from "react-helmet"
- import { useStaticQuery, StaticQuery, graphql } from "gatsby"
- 
- function SEO({ description, lang, meta, title, thumbnail, metaKeywords, pathname }) {
+ import { useStaticQuery, graphql } from "gatsby"
+ import MetaTags from 'react-meta-tags';
+ function SEO({ description, lang, meta, title, thumbnail, metaKeywords, slug }) {
    const { site } = useStaticQuery(
      graphql`
        query {
@@ -28,126 +28,45 @@
      `
    )
  
-  //  const metaDescription = description || site.siteMetadata.description
-  //  const metaThumbnail =  thumbnail || site.siteMetadata.thumbnail
+   const metaDescription = description || site.siteMetadata.description
+   const metaThumbnail =  thumbnail || site.siteMetadata.thumbnail
  
-  //  const keywords = metaKeywords ? metaKeywords : site.siteMetadata.keyWords
-  //  const url = site.siteMetadata.siteUrl
-  //  const defaultTitle = site.siteMetadata?.title
+   const keywords = metaKeywords ? metaKeywords : site.siteMetadata.keyWords
+   const url = site.siteMetadata.siteUrl
+   const defaultTitle = site.siteMetadata?.title
  
-  //  const titleUpperCase = title.replace(/\w\S*/g, w =>
-  //    w.replace(/^\w/, c => c.toUpperCase())
-  //  )
+   const titleUpperCase = title.replace(/\w\S*/g, w =>
+     w.replace(/^\w/, c => c.toUpperCase())
+   )
  
-  //  const canonical = pathname ? `${url}/${pathname}` : url
+   const metaTitle =  titleUpperCase || defaultTitle
+   const canonical = slug ? `${url}/${slug}` : url
  
    console.log('title', title)
    console.log('thumbnail1', thumbnail)
    console.log('thumbnail2', site.siteMetadata.thumbnail)
    
    return (
-     <StaticQuery
-      query={site}
-      render={data => {
-        const metaDescription = description || site.siteMetadata.description
-        const metaThumbnail =  thumbnail || site.siteMetadata.thumbnail
-      
-        const keywords = metaKeywords ? metaKeywords : site.siteMetadata.keyWords
-        const url = site.siteMetadata.siteUrl
-        const defaultTitle = site.siteMetadata?.title
-      
-        const titleUpperCase = title.replace(/\w\S*/g, w =>
-          w.replace(/^\w/, c => c.toUpperCase())
-        )
-      
-        const canonical = pathname ? `${url}/${pathname}` : url
-
-        return(
-          <Helmet
-            htmlAttributes={{
-              lang,
-            }}
-            title={titleUpperCase}
-            titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
-            link={
-              canonical
-                ? [
-                    {
-                      rel: "canonical",
-                      href: canonical,
-                    },
-                  ]
-                : []
-            }
-            meta={[
-              {
-                name: `description`,
-                content: metaDescription,
-              },
-              {
-                property: `og:image`,
-                content: metaThumbnail,
-              },
-              {
-                property: `og:image:secure_url`,
-                content: metaThumbnail,
-              },
-              {
-                property: `og:image:alt`,
-                content: defaultTitle,
-              },
-              {
-                property: `og:title`,
-                content: defaultTitle,
-              },
-              {
-                property: `og:description`,
-                content: metaDescription,
-              },
-              {
-                property: `og:type`,
-                content: `website`,
-              },
-              {
-                property: `og:url`,
-                content: pathname ? `${url}/${pathname.toLowerCase()}` : url,
-              },
-              {
-                name: `twitter:card`,
-                content: `summary`,
-              },
-              {
-                name: `twitter:creator`,
-                content: site.siteMetadata?.author || ``,
-              },
-              {
-                name: `twitter:title`,
-                content: titleUpperCase,
-              },
-              {
-                name: `twitter:description`,
-                content: metaDescription,
-              },
-              {
-                property: `twitter:image`,
-                content: metaThumbnail,
-              },
-              {
-                name: `robots`,
-                content: `index,follow`,
-              },
-              {
-                name: `keywords`,
-                content: keywords,
-              },
-            ].concat(meta)}
-          />
-        )
-      }}
-      />
+     <MetaTags>
+       <title>{defaultTitle ? `%s | ${defaultTitle}` : null}</title>
+       <meta name="description" content={metaDescription} />
+       <meta property="og:title" content={metaTitle} />
+       <meta property="og:image:secure_url" content={metaThumbnail} />
+       <meta property="og:image:alt" content={metaTitle} />
+       <meta property="og:description" content={metaDescription} />
+       <meta property="og:type" content="website" />
+       <meta property="og:url" content={slug ? `${url}/${slug.toLowerCase()}` : url} />
+       <meta property="twitter:card" content="summary" />
+       <meta property="twitter:creator" content={site.siteMetadata?.author || ""} />
+       <meta property="twitter:title" content={metaTitle} />
+       <meta property="twitter:description" content={metaDescription} />
+       <meta property="twitter:image" content={metaThumbnail} />
+       <meta property="robots" content="index,follow" />
+       <meta property="keywords" content={keywords} />
+       <link rel="canonical" href={canonical} />
+     </MetaTags>
    )
-    }
-      
+ }
  
  SEO.defaultProps = {
    lang: `en`,
@@ -155,7 +74,7 @@
    description: ``,
    thumbnail: ``,
    metaKeywords: ``,
-   pathname: ``,
+   slug: ``,
  }
  
  SEO.propTypes = {
@@ -165,7 +84,7 @@
    title: PropTypes.string.isRequired,
    thumbnail: PropTypes.string,
    metaKeywords: PropTypes.string,
-   pathname: PropTypes.string,
+   slug: PropTypes.string,
  }
  
  export default SEO
