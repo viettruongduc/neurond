687fd3a372187a4301b043495893d8cacdf205d3
viettruongduc <vietdt301@gmail.com>
2021-08-09 09:50:01 +0700

    add gatsby-ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,39 +1,12 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/ssr-apis/
- */
-
-// You can delete this file if you're not using it
 const React = require("react")
-const { Helmet } = require("react-helmet")
-
-exports.onRenderBody = (
-  { setHeadComponents, setHtmlAttributes, setBodyAttributes },
-  pluginOptions
-) => {
-  const helmet = Helmet.renderStatic()
-  setHtmlAttributes(helmet.htmlAttributes.toComponent())
-  setBodyAttributes(helmet.bodyAttributes.toComponent())
-  setHeadComponents([
-    helmet.title.toComponent(),
-    helmet.link.toComponent(),
-    helmet.meta.toComponent(),
-    helmet.noscript.toComponent(),
-    helmet.script.toComponent(),
-    helmet.style.toComponent(),
-  ])
-}
-
-exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
-  const headComponents = getHeadComponents()
-  headComponents.sort((x, y) => {
-    if (x.props && x.props["data-react-helmet"]) {
-      return -1
-    } else if (y.props && y.props["data-react-helmet"]) {
-      return 1
-    }
-    return 0
+const Layout = require("./src/components/layout")
+// Adds a class name to the body element
+exports.onRenderBody = ({ setBodyAttributes }, pluginOptions) => {
+  setBodyAttributes({
+    className: "my-body-class",
   })
-  replaceHeadComponents(headComponents)
+}
+// Wraps every page in a component
+exports.wrapPageElement = ({ element, props }) => {
+  return <Layout {...props}>{element}</Layout>
 }
