1e1bf5dc8ee669f9c4770394f4b3d32550ebc184
viettruongduc <vietdt301@gmail.com>
2021-08-18 10:40:52 +0700

    fix

--- src/components/seo.js
+++ src/components/seo.js
@@ -73,8 +73,8 @@ function SEO({ description, lang, meta, thumbnail, metaKeywords, title, pathname
             htmlAttributes={{
               lang,
             }}
-            title={defaultTitle}
-            titleTemplate={data.site.siteMetadata.title}
+            title={title}
+            titleTemplate={title}
             link={
               canonical
                 ? [
@@ -104,7 +104,7 @@ function SEO({ description, lang, meta, thumbnail, metaKeywords, title, pathname
               },
               {
                 property: `og:title`,
-                content: defaultTitle,
+                content: title,
               },
               {
                 property: `og:description`,
